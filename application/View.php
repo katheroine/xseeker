2b4a4656015604be44819da0601d4e6a379584bf
Katarzyna Krasińska <katheroine@gmail.com>
2014-01-13 23:00:06 +0100

    View class added

---
+++ application/View.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once __DIR__ . '/ViewHelper.php';
+
+/**
+ * The View class manages view files.
+ * 
+ * @package XSeeker
+ * @author Katarzyna Krasińska <katarzyna.krasinska@onet.pl>
+ */
+class View
+{
+	/**
+	 * Layout file name.
+	 */
+	const LAYOUT_FILE = 'layout.php';
+	
+	/**
+	 * Views directory.
+	 */
+	const VIEWS_DIRECTORY = 'views';
+	
+	/**
+	 * Data passed to the view.
+	 * 
+	 * @var array
+	 */
+	private $view_parameters = array();
+	
+	/*
+	public function __construct()
+	{
+		$this->initializeViewParameters();
+	}
+	
+	private function initializeViewParameters()
+	{
+		$this->view_parameters = array();
+	}
+	 */
+
+	public function __set($name, $value)
+	{
+		$this->view_parameters[$name] = $value;
+	}
+	
+	/**
+	 * Render view file specified by the argument.
+	 * 
+	 * @param string $view
+	 */
+	public function render($view)
+	{
+		foreach( $this->view_parameters as $parameter_key => $parameter_value )
+		{
+			$$parameter_key = $parameter_value;
+		}
+		
+		$view_path = self::bulidViewPath();
+		require_once $view_path;
+	}
+	
+	/**
+	 * Bulid view file path.
+	 * 
+	 * @return string
+	 */
+	private static function bulidViewPath()
+	{
+		return __DIR__ . '/' . self::VIEWS_DIRECTORY . '/' . self::LAYOUT_FILE;
+	}
+}
