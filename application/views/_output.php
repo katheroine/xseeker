4f71f7e21809710b510f7eb4d130054ad8345134
Katarzyna Krasińska <katheroine@gmail.com>
2014-01-13 22:45:46 +0100

    partial view output added

---
+++ application/views/_output.php
@@ -0,0 +1,13 @@
+<div id="output">
+<?php if(!empty($entry_items)) { ?>
+	<?php foreach($entry_items as $key => $entry_item) { ?>
+		<div class="entry">
+			<h1><a href="<?= $entry_item->link ?>"> <?= $entry_item->title ?> </a></h1>
+			<p> <?= ViewHelper::emphasizeKeywordInContent($keyword, $entry_item->description) ?></p>
+			<h2> <?= $entry_item->date ?> </h2>
+		</div>
+	<?php } ?>
+<?php } else { ?>
+	<div id="entry_not_found">Entry with keyword <span class="keyword"><?= $keyword ?></span> cannot be found.</div>
+<?php } ?>
+</div>
