3fbae8ba14d4d272b3e006733a73de215adae7e7
Katarzyna Krasińska <katheroine@gmail.com>
2014-01-13 22:44:22 +0100

    partial view welcome added

---
+++ application/views/_welcome.php
@@ -0,0 +1,5 @@
+<div id="welcome">
+	<h1>Welcome on <strong>XSeeker</strong>!</h1>
+	<p>We give you a quick access to <a href="http://xlab.pl/">XLab</a> RSS channel.</p>
+	<p>Enter the keyword and explore XLab entries!</p>
+</div>
